92c00fe7a0f0cddce4cc1df93d5e26bb4401d2c5
geekbrains <ashulyaev93@mail.ru>
2020-05-31 10:17:57 +0400

    Изменения для pull

--- src/Animals/Cat.java
+++ src/Animals/Cat.java
@@ -2,7 +2,7 @@ package Animals;
 
 public class Cat extends Animals
 {
-        //кот не умеет плавать
+        //кот не умеет плавать;
         public Cat(String name, int run, int jump)
         {
                 this.name = name;
